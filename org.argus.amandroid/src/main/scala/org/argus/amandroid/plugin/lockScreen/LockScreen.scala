a3564889b84c2d08f4b1db56e258f0f1fee62558
shivapbhusal <civabhusal@gmail.com>
2018-02-19 11:55:20 -0500

    edit comments

--- org.argus.amandroid/src/main/scala/org/argus/amandroid/plugin/lockScreen/LockScreen.scala
+++ org.argus.amandroid/src/main/scala/org/argus/amandroid/plugin/lockScreen/LockScreen.scala
@@ -13,9 +13,14 @@ class LockScreen(mainActivity:JawaType) {
 }
 
 /* Steps
+Iteration 1:
+
 1. Get the control flow graph from a given APK
 2. Check each of the nodes in the CFG
 3. If any of the nodes contain the malicious signature, set the Flag as true
 4. Return the Flag
-5. Test it with the test script 
+5. Test it with the test script
+
+Iteration 2:
+What if the signature is in a service but that service/ class is never started ?
  */
