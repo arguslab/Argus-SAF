a8321b44f72ddb325c7f8c62abee15ee3f838221
shivapbhusal <civabhusal@gmail.com>
2018-02-19 11:53:41 -0500

    add comments

--- org.argus.amandroid/src/main/scala/org/argus/amandroid/plugin/lockScreen/LockScreen.scala
+++ org.argus.amandroid/src/main/scala/org/argus/amandroid/plugin/lockScreen/LockScreen.scala
@@ -6,18 +6,16 @@ import org.argus.jawa.core.{Global, JawaType}
 class LockScreen(mainActivity:JawaType) {
 
   def checkLockScreen(global: Global, idfgOpt: Option[InterProceduralDataFlowGraph]): Boolean = {
-    var flag: Boolean = 0
-    val clazz = global.getClassOrResolve(mainActivity)
-    if (!clazz.isSystemLibraryClass && clazz.isConcrete) {
-      clazz.getDeclaredMethods
-      for each { method =>
-        val code = method.getBody.toCode
-        if (code.contains("Hello World")) {
-          flag = 1
-        }
-      }
-
-    }
+    var isFlag:Boolean=false
+    isFlag
   }
-  flag
+
 }
+
+/* Steps
+1. Get the control flow graph from a given APK
+2. Check each of the nodes in the CFG
+3. If any of the nodes contain the malicious signature, set the Flag as true
+4. Return the Flag
+5. Test it with the test script 
+ */
