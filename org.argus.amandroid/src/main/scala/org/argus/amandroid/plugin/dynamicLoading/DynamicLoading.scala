acf7daa34a509f1215c13dbe1064847000203a1c
shivapbhusal <civabhusal@gmail.com>
2018-03-05 14:34:09 -0500

    add bankbot test app

--- org.argus.amandroid/src/main/scala/org/argus/amandroid/plugin/dynamicLoading/DynamicLoading.scala
+++ org.argus.amandroid/src/main/scala/org/argus/amandroid/plugin/dynamicLoading/DynamicLoading.scala
@@ -1,5 +1,16 @@
 package org.argus.amandroid.plugin.dynamicLoading
 
+import org.argus.jawa.alir.dfa.InterProceduralDataFlowGraph
+import org.argus.jawa.core.Global
+
 class DynamicLoading {
+
+  def checkDynamicLoading(global: Global, idfgOpt: Option[InterProceduralDataFlowGraph]):Boolean ={
+    checkPresence()
+  }
+
+  def checkPresence():Boolean={
+    true
+  }
 
 }
--- org.argus.amandroid/src/test/scala/org/argus/amandroid/plugin/DynamicLoadingTest.scala
+++ org.argus.amandroid/src/test/scala/org/argus/amandroid/plugin/DynamicLoadingTest.scala
@@ -1,5 +1,37 @@
 package org.argus.amandroid.plugin
 
-class DynamicLoadingTest {
+import org.argus.amandroid.alir.componentSummary.ApkYard
+import org.argus.amandroid.core.decompile.{DecompileLayout, DecompileStrategy, DecompilerSettings}
+import org.argus.amandroid.plugin.dynamicLoading.DynamicLoading
+import org.argus.jawa.core.{MsgLevel, NoReporter, PrintReporter}
+import org.argus.jawa.core.util.FileUtil
+import org.scalatest.FlatSpec
+
+
+class DynamicLoadingTest extends FlatSpec{
+  private final val DEBUG=false
+
+  "App" should "be flagged yes" in {
+    val result:Boolean=testDynamicLoading(getClass.getResource("/apks/bankbot.apk").getPath)
+  }
+
+  private def testDynamicLoading(apkFile:String):Boolean=
+  {
+    val fileUri = FileUtil.toUri(apkFile)
+    val outputUri = FileUtil.toUri(apkFile.substring(0, apkFile.length - 4))
+    val reporter =
+      if(DEBUG) new PrintReporter(MsgLevel.INFO)
+      else new NoReporter
+    val yard = new ApkYard(reporter)
+    val layout = DecompileLayout(outputUri)
+    val strategy = DecompileStrategy(layout)
+    val settings = DecompilerSettings(debugMode = false, forceDelete = true, strategy, reporter)
+    val apk = yard.loadApk(fileUri, settings, collectInfo = false, resolveCallBack = false)
+
+    val checker=new DynamicLoading()
+    val res=checker.checkDynamicLoading(apk,None)
+    res
+  }
+
 
 }
