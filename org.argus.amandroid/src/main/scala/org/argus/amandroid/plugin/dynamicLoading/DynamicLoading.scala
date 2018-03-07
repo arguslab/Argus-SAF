5686735686e3390240702fba6a1f3fb744405b4e
shivapbhusal <civabhusal@gmail.com>
2018-03-06 20:00:48 -0500

    add draft -- Dynamic_loading checker

--- org.argus.amandroid/src/main/scala/org/argus/amandroid/plugin/dynamicLoading/DynamicLoading.scala
+++ org.argus.amandroid/src/main/scala/org/argus/amandroid/plugin/dynamicLoading/DynamicLoading.scala
@@ -5,12 +5,25 @@ import org.argus.jawa.core.Global
 
 class DynamicLoading {
 
-  def checkDynamicLoading(global: Global, idfgOpt: Option[InterProceduralDataFlowGraph]):Boolean ={
-    checkPresence()
-  }
+  def checkDynamicLoading(global: Global, idfgOpt: Option[InterProceduralDataFlowGraph]): Boolean = {
+    var hasDynamicLoading: Boolean = false
+    global.getApplicationClassCodes foreach { case (typ, f) =>
+      if ((f.code.contains("Landroid/view/WindowManager$LayoutParams;")) && (hasLockScreen == false)) {
+        global.getClazz(typ) match {
+          case Some(c) =>
+            c.getDeclaredMethods.foreach { x =>
+              if (hasDynamicLoading == false) {
+                hasDynamicLoading = checkPresence()
+              }
+            }
+        }
+      }
+      hasDynamicLoading
+    }
 
-  def checkPresence():Boolean={
-    true
-  }
+    def checkPresence(): Boolean = {
+      true
+    }
 
+  }
 }
